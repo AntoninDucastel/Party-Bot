3ea9b980fc56d13f8f4359db3bfde2982aa305e4
AntoninDucastel <37896584+AntoninDucastel@users.noreply.github.com>
2018-05-26 23:28:35 +0200

    Create MainBot.java

---
+++ MainBot.java
@@ -0,0 +1,63 @@
+package event;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
+
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import command.CommandMap;
+import net.dv8tion.jda.core.AccountType;
+
+public class MainBot implements Runnable{
+	
+	private final JDA jda;
+	private final CommandMap commandMap = new CommandMap(this);
+	private final Scanner scanner = new Scanner(System.in);
+	private boolean running;
+	
+	public MainBot() throws LoginException {
+		jda = new JDABuilder(AccountType.BOT).setToken("NDI2ODQwMjMzMDU3OTc2MzIw.DZfYgA.0eWE27cZyupeGmX_oXu3JkXJQBc").buildAsync();
+		jda.addEventListener(new BotListener(commandMap));
+		System.out.println("Hey, je suis Party Bot !");
+		System.out.println("Bot Party est dans la place !");
+		
+		String builder = "=help";
+		jda.getPresence().setGame(Game.playing(builder.toString()));
+		
+	}
+	
+	public JDA getJda() {
+		return jda;
+	}
+	
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+	
+	@Override
+	public void run() {
+		running = true;
+		while (running) {
+			if(scanner.hasNextLine()) commandMap.commandConsole(scanner.nextLine());
+		}
+		
+		scanner.close();
+		System.out.println("Bot hors tension.");
+		jda.shutdown(); //FALSE !!!
+		commandMap.save();
+		System.exit(0);
+	}
+	
+	public static void main (String[] args) {
+		try {
+			MainBot mainBot = new MainBot();
+			new Thread(mainBot, "Party Bot").start();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
